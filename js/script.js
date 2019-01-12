8071121356bbad14d3638a856c15741ed47ad763
acandael <info@anthonycandaele.com>
2019-01-12 14:25:50 +0100

    change color options dynamically

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 $( document ).ready(function() {
+  // set focus to the first text field
   $('#name').focus();
+
+  // show text field when 'Other' is selected from Job Role
+  $('#title').change(function(e) {
+    if (e.target.value === 'other') {
+      $('#other-text').show();
+    }
+  });
+
+  // show color options that match the design
+  $('#design').on('change', function(e){
+    if (e.target.value === 'js puns') {
+      $('#color option').each(function(index, value) {
+        if (this.value === 'cornflowerblue' | this.value === 'darkslategrey' | this.value ==='gold') {
+          $(this).show();
+        }
+        else {
+          $(this).hide();
+        }
+      });
+    } else {
+      $('#color option').each(function(index, value){
+        if (this.value === 'tomato' | this.value === 'steelblue' | this.value === 'dimgrey') {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    }
+  });
 });
