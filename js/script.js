47b72d9182eaf4ea3e579acfc6a7b753e83a0d27
acandael <info@anthonycandaele.com>
2019-01-14 19:46:23 +0100

    refactor dynamic drop down and add comments

--- js/script.js
+++ js/script.js
@@ -9,32 +9,33 @@ $( document ).ready(function() {
     }
   });
 
-  $('#color').hide();
+  
   // show color options that match the design
   $('#design').change(function(e){
     
     if (e.target.value === 'js puns') {
+      // set the color options for the 'js puns' design
       $('#color option').each(function() {
         if (this.value === 'cornflowerblue' | this.value === 'darkslategrey' | this.value ==='gold') {
           $(this).show();
         } else {
           $(this).hide();
         }
+        // set selected option
         $('#color').val('cornflowerblue');
       });
     } else if (e.target.value === 'heart js') {
-      
+      // set the color options for the 'heart js' design
       $('#color option').each(function() {
         if (this.value === 'tomato' | this.value === 'steelblue' | this.value === 'dimgrey') {
           $(this).show();
         } else {
           $(this).hide();
         }
+        // set selected option
         $('#color').val('tomato');
       });
     }
-   
-    $('#color').show();
     
   });
 });
