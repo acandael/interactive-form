7cbcf59ed8aaf5d3e206bce2d277914b9242b1df
acandael <info@anthonycandaele.com>
2019-01-19 13:28:48 +0100

    stuck at the validation that at least one activity should be checked

--- js/script.js
+++ js/script.js
@@ -39,6 +39,9 @@ $( document ).ready(function() {
     
   });
 
+  // Register For Activities
+  //  If the user selects a workshop, don't allow selection of a workshop at the same day and       time
+
   $('.activities :checkbox').on("change", function(){
     // set up variables for the labels
     let labelText = '';
@@ -47,13 +50,15 @@ $( document ).ready(function() {
 
     // the event was checked
     if (this.checked) {
+      // add check attribute to checkbox
+      $(this).attr("checked", true);
       const checked = $(this).prop("labels");
       // get the text from the label and parse it for the date part
       labelText = $(checked).text().split('—')[1];
       
       // disable events with the same date
       $('.activities :checkbox').each(function(key, value){
-        
+
         otherLabel = $(value).prop("labels");
         otherText = $(otherLabel).text().split('—')[1];
         
@@ -84,6 +89,90 @@ $( document ).ready(function() {
       });
     }
   });
-  
+
+  // Payment Info
+  // Display payment sections based on the payment option chosen in the select menu.
+
+  //The "Credit Card" payment option should be selected by default
+  $('#payment').val('credit card');
+  $('#credit-card').show();
+  $('#paypal').hide();
+  $('#bitcoin').hide();
+  $('#noPaymentInfo').hide();
+
+  // show the payment information based on the selected payment option
+  $('#payment').on('change', function(e){
+    const option = e.target.value;
+    switch (option) {
+      case 'paypal':
+        $('#paypal').show();
+        $('#bitcoin').hide();
+        $('#credit-card').hide();
+        break;
+      case 'bitcoin':
+        $('#bitcoin').show();
+        $('#paypal').hide();
+        $('#credit-card').hide();
+        break;
+      case 'credit card':
+        $('#credit-card').show();
+        $('#paypal').hide();
+        $('#bitcoin').hide();
+        break;
+    }
+  });
+
+  // Validate the form when submit button is clicked
+  $('form').on('submit', function(e){
+    e.preventDefault();
+
+    // The user should select a payment option
+    if ($('#payment').val() === 'select_method') {
+      $('#noPaymentInfo').show();
+    }
+
+    // The name field can't be blank
+    if (!$('#name').val()) {
+      $('#name').after("<p class='error'>The name field can not be blank!</p>");
+      $('#name').addClass('error-field');
+    }
+
+    // At least one activity must be checked
+    if (isActivityChecked()) {
+      $('.activities').after("<p class='error'>At least one activity must be checked</p>");
+    } else {
+      $('.activities p.error').remove();
+    }
+
+    
+  });
+
+  $('#mail').change(function(e){
+    const mail = $('#mail').val();
+    if (!isValidEmail(mail)) {
+      $('#mail').after("<p class='error'>The format of the email is not valid</p>");
+      $('#mail').addClass('error-field');
+    } else {
+      $('#mail').next('p').remove();
+    }
+  })
+
+  function isValidEmail(email) {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+  }
+
+  function isActivityChecked() {
+    $('.activities :checkbox').each(function(key, value) {
+      let isChecked = false;
+      
+      if (value.checked) {
+        isChecked = true;
+        console.log('activity was checked');
+      }
+
+      return isChecked;
+    });
+  }
+
 });
 
